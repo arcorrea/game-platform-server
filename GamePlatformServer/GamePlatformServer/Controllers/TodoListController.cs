f9db771ff1409ee6caa060aba990721014366f25
Adrián Rivera <rivera.correa88@hotmail.com>
2022-10-12 21:25:23 -0500

    Validate Azure AD B2C token

---
+++ GamePlatformServer/GamePlatformServer/Controllers/TodoListController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace GamePlatformServer.Controllers
+{
+    [Authorize]
+    [RequiredScope("tasks.read")]
+    [ApiController]
+    [Route("game-api/[controller]")]
+    public class TodoListController : ControllerBase
+    {
+        private readonly ILogger<TodoListController> _logger;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TodoListController(ILogger<TodoListController> logger, IHttpContextAccessor contextAccessor)
+        {
+            _logger = logger;
+            _contextAccessor = contextAccessor;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var todoList = new List<string>()
+            {
+                "Task1",
+                "Task2",
+                "Task3"
+            };
+            return Ok(todoList);
+        }
+
+    }
+}
--- GamePlatformServer/GamePlatformServer/Program.cs
+++ GamePlatformServer/GamePlatformServer/Program.cs
@@ -1,6 +1,35 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+var corsPolicyName = "AllowAngularApp";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: corsPolicyName,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+#endregion
+
+#region Microsoft Identity platform (Azure AD B2C)
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApi(options =>
+        {
+            builder.Configuration.Bind("AzureAdB2C", options);
+
+            options.TokenValidationParameters.NameClaimType = "name";
+        },
+options => { builder.Configuration.Bind("AzureAdB2C", options); });
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +46,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseCors(corsPolicyName);
 
 app.UseAuthorization();
 
